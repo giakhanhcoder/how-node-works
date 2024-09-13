c5238305ac9364285a30536216ca9f1a9c88fabc
GIA KHANH <giakhanh27403@gmail.com>
2024-09-13 11:43:35 +0700

    streaming nodejs

---
+++ events.js
@@ -0,0 +1,48 @@
+const EventEmitter = require("events");
+const http = require("http");
+const { hostname } = require("os");
+
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+const myEmitter = new Sales();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale!");
+});
+
+myEmitter.on("newSale", () => {
+  console.log("Customer name: VGK");
+});
+
+myEmitter.on("newSale", (stock, freeship) => {
+  console.log(
+    `There are now ${stock} items left in stock. And ${freeship} items`
+  );
+});
+
+myEmitter.emit("newSale", 9, "free ship");
+
+//////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  console.log(req.url);
+  res.end("Request received!");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another request received!");
+});
+
+server.on("close", () => {
+  console.log("Server closed!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
